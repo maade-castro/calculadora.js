12c1dee84ed82208643a1e431902936695e1d40e
maade-castro <madeikys1995@gmail.com>
2023-10-08 14:40:19 -0300

    escucha del teclado del dispositivo

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ function calcular (){
     const resultado = eval(valorPantalla)
     document.getElementById('pantalla').value = resultado 
 }
+
+
+// Agrega un evento de escucha de teclado al campo de entrada de texto
+document.getElementById('pantalla').addEventListener('keydown', function(event) {
+    // Obtiene el valor de la tecla presionada
+    const keyValue = event.key;
+
+    // Verifica si la tecla presionada es un número, un operador válido o la tecla "Backspace"
+    if (/[\d\+\-\*\/\.]/.test(keyValue)) {
+        // Agrega la tecla presionada a la pantalla
+        agregar(keyValue);
+    } else if (keyValue === 'Enter') {
+        // Si se presiona la tecla "Enter", calcula el resultado
+        calcular();
+    } else if (keyValue === 'Escape') {
+        // Si se presiona la tecla "Escape", borra la pantalla
+        borrar();
+    } else if (keyValue === 'Backspace') {
+        // Si se presiona la tecla "Backspace", borra el último carácter en la pantalla
+        const pantalla = document.getElementById('pantalla');
+        pantalla.value = pantalla.value.slice(0, -1);
+    }
+});
+
